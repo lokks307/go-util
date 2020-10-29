b61f36f50851319f6ebeda4c5ddd881f6e3107b2
Jeonil Kang <tethys.jeonilkang@gmail.com>
2020-10-29 19:05:55 +0900

    # EDIT - add funs for bytesbuilder

--- bytesbuilder/bytesbuilder.go
+++ bytesbuilder/bytesbuilder.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"reflect"
+	"strings"
 
 	"github.com/btcsuite/btcutil/base58"
 )
@@ -14,84 +15,161 @@ type ByteBuilder struct {
 }
 
 func NewBuilder() *ByteBuilder {
-	b := ByteBuilder{}
-	return &b
+	return &ByteBuilder{}
 }
 
-func (builder *ByteBuilder) Clear() {
-	builder.BytesArr = nil
+func (m *ByteBuilder) Clear() {
+	m.BytesArr = nil
 }
 
-//TODO : exception handling
-
-func (builder *ByteBuilder) Append(data interface{}) {
+func (m *ByteBuilder) Append(data ...interface{}) {
 	var byteData []byte
-	switch t := data.(type) {
-	case bool:
-		if t {
-			byteData = append(byteData, 0x01)
-		} else {
-			byteData = append(byteData, 0x00)
+
+	for idx := range data {
+		byteData = nil
+		switch t := data[idx].(type) {
+		case bool:
+			if t {
+				byteData = append(byteData, 0x01)
+			} else {
+				byteData = append(byteData, 0x00)
+			}
+		case string:
+			byteData = []byte(t)
+		case uint8:
+			byteData = append(byteData, t)
+		case []uint8: // == []byte
+			byteData = t
+		case int16, uint16:
+			var v uint16
+			if reflect.TypeOf(t).Kind() == reflect.Int16 {
+				v = uint16(t.(int16))
+			} else {
+				v = t.(uint16)
+			}
+			b := make([]byte, 2)
+			binary.BigEndian.PutUint16(b, v)
+			byteData = b
+		case int32, uint32:
+			var v uint32
+			if reflect.TypeOf(t).Kind() == reflect.Int32 {
+				v = uint32(t.(int32))
+			} else {
+				v = t.(uint32)
+			}
+			b := make([]byte, 4)
+			binary.BigEndian.PutUint32(b, v)
+			byteData = b
+		case int64, uint64:
+			var v uint64
+			if reflect.TypeOf(t).Kind() == reflect.Int64 {
+				v = uint64(t.(int64))
+			} else {
+				v = t.(uint64)
+			}
+			b := make([]byte, 8)
+			binary.BigEndian.PutUint64(b, v)
+			byteData = b
 		}
-	case string:
-		byteData = []byte(t)
-	case uint8:
-		byteData = append(byteData, t)
-	case []uint8: // == []byte
-		byteData = t
-	case int16, uint16:
-		var v uint16
-		if reflect.TypeOf(t).Kind() == reflect.Int16 {
-			v = uint16(t.(int16))
-		} else {
-			v = t.(uint16)
+
+		if byteData != nil {
+			m.BytesArr = append(m.BytesArr, byteData...)
 		}
-		b := make([]byte, 2)
-		binary.BigEndian.PutUint16(b, v)
-		byteData = b
-	case int32, uint32:
-		var v uint32
-		if reflect.TypeOf(t).Kind() == reflect.Int32 {
-			v = uint32(t.(int32))
-		} else {
-			v = t.(uint32)
-		}
-		b := make([]byte, 4)
-		binary.BigEndian.PutUint32(b, v)
-		byteData = b
-	case int64, uint64:
-		var v uint64
-		if reflect.TypeOf(t).Kind() == reflect.Int64 {
-			v = uint64(t.(int64))
-		} else {
-			v = t.(uint64)
-		}
-		b := make([]byte, 8)
-		binary.BigEndian.PutUint64(b, v)
-		byteData = b
 	}
-	builder.BytesArr = append(builder.BytesArr, byteData...)
 }
 
-func (builder *ByteBuilder) AppendHex(hexStr string) {
-	data, _ := hex.DecodeString(hexStr[2:])
-	builder.BytesArr = append(builder.BytesArr, data...)
+func (m *ByteBuilder) AppendHex(hexStrArr ...string) {
+	for idx := range hexStrArr {
+		if hexStrArr[idx] == "" {
+			continue
+		}
+
+		if strings.HasPrefix(hexStrArr[idx], "0x") {
+			if data, err := hex.DecodeString(hexStrArr[idx][2:]); err == nil {
+				m.BytesArr = append(m.BytesArr, data...)
+			}
+		} else {
+			if data, err := hex.DecodeString(hexStrArr[idx]); err == nil {
+				m.BytesArr = append(m.BytesArr, data...)
+			}
+		}
+	}
 }
 
-func (builder *ByteBuilder) AppendBase64(b64Str string) {
-	decodedData, _ := base64.StdEncoding.DecodeString(b64Str)
-	builder.BytesArr = append(builder.BytesArr, decodedData...)
+func (m *ByteBuilder) AppendBase64(b64StrArr ...string) {
+	for idx := range b64StrArr {
+		if b64StrArr[idx] == "" {
+			continue
+		}
+
+		if decodedData, err := base64.StdEncoding.DecodeString(b64StrArr[idx]); err == nil {
+			m.BytesArr = append(m.BytesArr, decodedData...)
+		}
+	}
 }
 
-func (builder *ByteBuilder) AppendBase58(b58Str string) {
-	decodedData := base58.Decode(b58Str)
-	builder.BytesArr = append(builder.BytesArr, decodedData...)
+func (m *ByteBuilder) AppendBase58(b58StrArr ...string) {
+	for idx := range b58StrArr {
+		if b58StrArr[idx] == "" {
+			continue
+		}
+
+		m.BytesArr = append(m.BytesArr, base58.Decode(b58StrArr[idx])...)
+	}
 }
 
-func (builder *ByteBuilder) GetBytes() []byte {
-	return builder.BytesArr
+// old style
+
+func (m *ByteBuilder) GetBytes() []byte {
+	return m.BytesArr
 }
 
-func (builder *ByteBuilder) GetString() string {
-	return string(builder.BytesArr)
+func (m *ByteBuilder) GetString() string {
+	return m.String()
+}
+
+// new style
+
+func (m *ByteBuilder) Bytes() []byte {
+	return m.BytesArr
+}
+
+func (m *ByteBuilder) String() string {
+	if m.BytesArr == nil {
+		return ""
+	}
+
+	return string(m.BytesArr)
+}
+
+func (m *ByteBuilder) Len() int {
+	if m.BytesArr == nil {
+		return 0
+	}
+
+	return len(m.BytesArr)
+}
+
+func (m *ByteBuilder) Base58() string {
+	if m.BytesArr == nil {
+		return ""
+	}
+
+	return base58.Encode(m.BytesArr)
+}
+
+func (m *ByteBuilder) Base64() string {
+	if m.BytesArr == nil {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString(m.BytesArr)
+}
+
+func (m *ByteBuilder) Hex() string {
+	if m.BytesArr == nil {
+		return ""
+	}
+
+	return hex.EncodeToString(m.BytesArr)
 }
