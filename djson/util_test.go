a783d1584564bef4deb4d4be83f16d7258d89648
Jeonil Kang <53741382+dreamx-one@users.noreply.github.com>
2021-07-21 12:58:16 +0900

    # FIX - put nested array

--- djson/util_test.go
+++ djson/util_test.go
@@ -10,3 +10,9 @@ func TestTokenizer(t *testing.T) {
 	log.Println(PathTokenizer(`["aa"][1][b_b]`))  // [aa 1 b_b]
 	log.Println(PathTokenizer(`["a'a"][1][b]b]`)) // [a'a 1 b]
 }
+
+func TestParse(t *testing.T) {
+	doc := `[[1,2,3]]`
+	tdjson := NewDJSON().Parse(doc)
+	log.Println(tdjson.GetAsIntPath(`[0][0]`))
+}
